de2bc4eb7c4bc2ffdfa6057a8810054a7714b967
admin <matevosyan.2011@gmail.com>
2025-04-30 21:36:39 +0400

    changed axios instance config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-const axiosServices = axios.create();
+const axiosServices = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
 // ==============================|| AXIOS - FOR MOCK SERVICES ||============================== //
 
