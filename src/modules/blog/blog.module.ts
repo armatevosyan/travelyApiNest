0ee231aa0312c878e916d43b28f517b18896d1d0
admin <matevosyan.2011@gmail.com>
2025-11-16 12:55:45 +0400

    added blog module

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -3,15 +3,17 @@ import {
   Module,
   NestModule,
   RequestMethod,
-} from '@nestjs/common';
+ } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { Blog } from './blog.entity';
 import { AuthMiddleware } from '@/common/middleware/auth.middleware';
+import { UsersModule } from '@/modules/users/users.module';
+import { CategoryModule } from '@/modules/categories/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Blog])],
+  imports: [TypeOrmModule.forFeature([Blog]), UsersModule, CategoryModule],
   providers: [BlogService],
   controllers: [BlogController],
   exports: [BlogService, TypeOrmModule],
