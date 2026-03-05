2a12d7df11ccdedda97b223b4b70c41e66cf429f
admin <matevosyan.2011@gmail.com>
2026-03-05 21:31:32 +0400

    added push notifications

---
+++ sendPushNotification.js
@@ -0,0 +1,26 @@
+import { Expo } from 'expo-server-sdk';
+const expo = new Expo();
+
+const sendPushNotification = async (
+  token = 'ExponentPushToken[ipwbv7FHm_Jt8_v36Nhu7t]',
+  title,
+  body,
+) => {
+  if (!Expo.isExpoPushToken(token)) {
+    throw new Error(`Invalid Expo push token: ${token}`);
+  }
+
+  const messages = [
+    {
+      to: token,
+      sound: 'notification_main.wav',
+      title: 'New Booking',
+      subtitle: 'Place your booking here!',
+      body: 'You have a new booking request!',
+      data: { type: 'booking' },
+    },
+  ];
+
+  await expo.sendPushNotificationsAsync(messages);
+};
+sendPushNotification();
